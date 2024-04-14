4e3e8e8ebefe496980eb8d2385a59094f2bf6c92
Lucas Ribeiro <67128586+lucasrsilvak@users.noreply.github.com>
2024-04-14 14:20:17 -0300

    Update mean_4_clocks.vhd

--- aulas/lab_6/mean_4_clocks.vhd
+++ aulas/lab_6/mean_4_clocks.vhd
@@ -1,44 +1,37 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
 use ieee.numeric_std.all;
 
 entity mean_4_clocks is
     generic (
-        W       :       integer := 4
+        W       : integer := 4  -- Width of the input
     );
     port (
-        CLK     : in    std_logic;
-        RESET   : in    std_logic;
-        INPUT   : in    std_logic_vector(W - 1 downto 0);
-        OUTPUT  : out   std_logic_vector(W - 1 downto 0)
+        CLK     : in  std_logic;
+        RESET   : in  std_logic;
+        INPUT   : in  std_logic_vector(W - 1 downto 0);
+        OUTPUT  : out std_logic_vector(W - 1 downto 0)
     );
 end mean_4_clocks;
 
 architecture arch of mean_4_clocks is
+    signal var1, var2, var3, var4 : unsigned(W + 1 downto 0) := (others => '0');
+    signal sum : unsigned(W + 1 downto 0) := (others => '0');
 begin
-    process(CLK, RESET) is
-        variable var1 : unsigned(W - 1 downto 0);
-        variable var2 : unsigned(W - 1 downto 0);
-        variable var3 : unsigned(W - 1 downto 0);
-        variable var4 : unsigned(W - 1 downto 0);
-		  variable sum  : unsigned(W - 1 downto 0);
+    process(CLK, RESET)
     begin
-        if (RESET = '1') then
-            var1 := to_unsigned(0,W);
-            var2 := to_unsigned(0,W);
-            var3 := to_unsigned(0,W);
-            var4 := to_unsigned(0,W);
-        elsif (rising_edge(CLK)) then
-            var1 := unsigned("00" & INPUT(W-1 downto 2));
-            var2 := var1;
-            var3 := var2;
-            var4 := var3;
+        if RESET = '1' then
+            var1 <= (others => '0');
+            var2 <= (others => '0');
+            var3 <= (others => '0');
+            var4 <= (others => '0');
+        elsif rising_edge(CLK) then
+            var1 <= unsigned("00" & INPUT);
+            var2 <= var1;
+            var3 <= var2;
+            var4 <= var3;
         end if;
-        
-        sum := ((var1 + var2)+(var3 + var4));--Unica alterção de soma
-		  
-		  OUTPUT <= std_logic_vector(sum srl 2); -- divisão por 4
-		  
     end process;
-    
+	 sum <= (var1 + var2) + (var3 + var4);
+    OUTPUT <= std_logic_vector((sum(W+1 downto 2)));
 end arch;
